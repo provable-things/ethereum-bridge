dd43b3f770637096e165ca4b5b3a6907def7b231
nya1 <nya1@protonmail.com>
2016-12-15 13:37:04 +0100

    Try-catch on HTTP requests

--- plugin.js
+++ plugin.js
@@ -517,19 +517,31 @@ function OARgenerate(){
 
 function createQuery(query, callback){
   request.post('https://api.oraclize.it/v1/query/create', {body: query, json: true, headers: { 'X-User-Agent': BRIDGE_NAME+'/'+BRIDGE_VERSION+' (nodejs)' }}, function (error, response, body) {
-    if (error) console.error(error);
-    if (response.statusCode == 200) {
-      callback(body);
-    } else console.error("UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST "+BRIDGE_NAME.toUpperCase());
+    if (error) {
+      console.error("request error ",error);
+      schedule.scheduleJob(new Date(parseInt(Date.now()+5000)),function(){
+        var newTime = toPositiveNumber(query.when-5);
+        query.when = newTime;
+        createQuery(query,callback);
+      });
+    } else {
+      if (response.statusCode == 200) {
+        callback(body);
+      } else console.error("UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST "+BRIDGE_NAME.toUpperCase());
+    }
   });
 }
 
 function queryStatus(query_id, callback){
   request.get('https://api.oraclize.it/v1/query/'+query_id+'/status', {json: true, headers: { 'X-User-Agent': BRIDGE_NAME+'/'+BRIDGE_VERSION+' (nodejs)' }}, function (error, response, body) {
-    if (error) console.error(error);
-    if (response.statusCode == 200) {
-      callback(body);
-    } else console.error("UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST "+BRIDGE_NAME.toUpperCase());
+    if (error) {
+      console.error("request error ",error);
+      callback({"result":{}});
+    } else {
+      if (response.statusCode == 200) {
+        callback(body);
+      } else console.error("UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST "+BRIDGE_NAME.toUpperCase());
+    }
   });
 }
 
@@ -613,6 +625,7 @@ function runLog(){
     console.log(JSON.stringify(query));
     if(!myIdList[myIdInitial] && counter>0 || myIdList[myIdInitial]) return;
     createQuery(query, function(data){
+      if(typeof data.result.id === undefined && counter<=10) return;
       counter++;
       console.log("Query : "+JSON.stringify(data)); 
       myid = data.result.id;
@@ -720,9 +733,14 @@ function loadHandler(){
 
 function getQueryUnixTime(time,unixTime){
   if(time<unixTime && time>1420000000) return 0;
-  if(time<1420000000 && time>5) return parseInt(unixTime+time);
-  if(time>1420000000) return parseInt(time);
+  if(time<1420000000 && time>5) return toPositiveNumber(unixTime+time);
+  if(time>1420000000) return toPositiveNumber(time);
   return 0;
+}
+
+function toPositiveNumber(number){
+  if(number<0) return 0;
+  else return parseInt(number);
 }
 
 function checkQueryStatus(queryDoc,myid,myIdInitial,contractAddress,proofType,gasLimit){
