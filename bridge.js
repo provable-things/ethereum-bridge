4b1129cba48ff4fd908e5e8aaf80d1d0de403aa4
nya1 <nya1@protonmail.com>
2017-02-13 18:42:50 +0100

    Remove pending queries delay when a query is skipped

--- bridge.js
+++ bridge.js
@@ -417,8 +417,10 @@ function processPendingQueries (oar, connector, cbAddress) {
         logger.warn('forcing the resume of all pending queries')
       }
       asyncLoop(pendingQueries, function (thisPendingQuery, next) {
-        if (thisPendingQuery.callback_error === true) logger.warn('skipping', thisPendingQuery.contract_myid, 'because of __callback tx error')
-        else if (thisPendingQuery.retry_number < 3 || resumeQueries) {
+        if (thisPendingQuery.callback_error === true) {
+          logger.warn('skipping', thisPendingQuery.contract_myid, 'because of __callback tx error')
+          return next(null)
+        } else if (thisPendingQuery.retry_number < 3 || resumeQueries) {
           var targetUnix = parseInt(thisPendingQuery.target_timestamp)
           var queryTimeDiff = targetUnix < parseInt(Date.now() / 1000) ? 0 : targetUnix
           logger.info('re-processing query', {'contact_address:': thisPendingQuery.contact_address, 'contact_myid': thisPendingQuery.contract_myid, 'http_myid': thisPendingQuery.http_myid})
@@ -428,10 +430,13 @@ function processPendingQueries (oar, connector, cbAddress) {
             var targetDate = new Date(targetUnix * 1000)
             processQueryInFuture(targetDate, thisPendingQuery)
           }
-        } else logger.warn('skipping', thisPendingQuery.contract_myid, 'query, exceeded 3 retries')
+        } else {
+          logger.warn('skipping', thisPendingQuery.contract_myid, 'query, exceeded 3 retries')
+          return next(null)
+        }
         setTimeout(function () {
           next(null)
-        }, 1001)
+        }, 1000)
       }, function (err) {
         if (err) logger.error('Pending query error', err)
       })
@@ -995,7 +1000,7 @@ function queryComplete (gasLimit, myid, result, proof, contractAddr, proofType) 
       if (findErr !== null) return queryCompleteErrors(findErr)
       if (alreadyCalled === true) return queryCompleteErrors('queryComplete error, __callback for contract myid', myid, 'was already called before, skipping...')
       var callbackData = bridgeUtil.callbackTxEncode(myid, result, proof, proofType)
-      logger.info('sending __callback tx...')
+      logger.info('sending __callback tx...', {"contract_myid": myid, "contract_address": contractAddr})
       activeOracleInstance.sendTx({'from': activeOracleInstance.account, 'to': bridgeCore.ethUtil.addHexPrefix(contractAddr), 'gas': gasLimit, 'data': callbackData}, function (err, contract) {
         var callbackObj = {'myid': myid, 'result': result, 'proof': proof}
         if (err) {
@@ -1099,7 +1104,7 @@ function createQuery (query, callback) {
     } else {
       if (response.statusCode === 200) {
         callback(body)
-      } else console.error('UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST ' + BRIDGE_NAME.toUpperCase())
+      } else logger.error('UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST ' + BRIDGE_NAME.toUpperCase())
     }
   })
 }
@@ -1112,7 +1117,7 @@ function queryStatus (queryId, callback) {
     } else {
       if (response.statusCode === 200) {
         callback(body)
-      } else console.error('UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST ' + BRIDGE_NAME.toUpperCase())
+      } else logger.error('UNEXPECTED ANSWER FROM THE ORACLIZE ENGINE, PLEASE UPGRADE TO THE LATEST ' + BRIDGE_NAME.toUpperCase())
     }
   })
 }
