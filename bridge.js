bdcf0f1f17d151cf5e19e7dc92e5a07d31e28565
nya1 <nya1@protonmail.com>
2017-03-03 16:33:12 +0100

    Remove event check before __callback tx

--- bridge.js
+++ bridge.js
@@ -969,11 +969,12 @@ function checkCallbackTx (myid, callback) {
     if (res === null) return callback(new Error('queryComplete error, query with contract myid ' + myid + ' not found in database'), null)
     if (typeof res.callback_complete === 'undefined') return callback(new Error('queryComplete error, query with contract myid ' + myid), null)
     if (res.callback_complete === true) return callback(null, true)
-    else {
+    else return callback(null, false)
+    /*else {
       var eventTx = BlockchainInterface().inter.getTransaction(res.event_tx)
       if (eventTx === null || eventTx.blockHash === null || eventTx.blockHash !== res.block_tx_hash) return callback(new Error('queryComplete error, query with contract myid ' + myid + ' mismatch with block hash stored'), null)
       return callback(null, false)
-    }
+    }*/
   })
 }
 
