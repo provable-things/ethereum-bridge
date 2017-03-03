668ac5d9a0963b00386714aae4fff77db41c5810
nya1 <nya1@protonmail.com>
2017-03-03 11:22:58 +0100

    Check if transaction is null

--- bridge.js
+++ bridge.js
@@ -971,7 +971,7 @@ function checkCallbackTx (myid, callback) {
     if (res.callback_complete === true) return callback(null, true)
     else {
       var eventTx = BlockchainInterface().inter.getTransaction(res.event_tx)
-      if (typeof eventTx === 'undefined' || eventTx.blockHash === null || eventTx.blockHash !== res.block_tx_hash) return callback(new Error('queryComplete error, query with contract myid ' + myid + ' mismatch with block hash stored'), null)
+      if (eventTx === null || eventTx.blockHash === null || eventTx.blockHash !== res.block_tx_hash) return callback(new Error('queryComplete error, query with contract myid ' + myid + ' mismatch with block hash stored'), null)
       return callback(null, false)
     }
   })
