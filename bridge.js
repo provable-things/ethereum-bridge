e9109166eecaf07c7b7fedc729fcbf6cb5b852a0
nya1 <nya1@protonmail.com>
2017-03-15 21:27:47 +0100

    Reduce __callback invalid timeout

--- bridge.js
+++ bridge.js
@@ -1116,16 +1116,16 @@ function checkCallbackTxs () {
               logger.info('transaction hash', transaction.tx_hash, 'was confirmed in block', txContent.blockHash)
               return next(null)
             })
-          } else if ((moment().unix() - moment(transaction.timestamp_db).unix()) > 600) {
+          } else if ((moment().unix() - moment(transaction.timestamp_db).unix()) > 300) {
             Query.findOne({where: {'contract_myid': transaction.contract_myid, 'oar': activeOracleInstance.oar, 'cbAddress': activeOracleInstance.account}}, function (errQuery, contractInfo) {
               if (errQuery) return next(errQuery)
               if (contractInfo === null) return next(null)
-              logger.warn('__callback transaction', transaction.tx_hash, 'not confirmed after 10 minutes')
+              logger.warn('__callback transaction', transaction.tx_hash, 'not confirmed after 5 minutes, resuming query with contract myid', contractInfo.contract_myid)
               contractInfo.force_query = true
-              var preCallbackUpdate = {'$set': {'timestamp_db': moment().unix() * 1000}}
+              var preCallbackUpdate = {'$set': {'timestamp_db': (moment().unix() * 1000)}}
               CallbackTx.update({where: {'tx_hash': transaction.tx_hash}}, preCallbackUpdate, function (errUpdate, resUpdate) {
                 checkQueryStatus(contractInfo)
-                next(null)
+                return next(null)
               })
             })
           } else return next(null)
